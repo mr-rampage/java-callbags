4b804d27e541ffcee90360e718991caa517cf54d
mr-rampage <readonly@che.wbac.ca>
2018-10-27 01:23:34 -0400

    chore(maven) setting up maven

--- src/test/java/e2eTest.java
+++ src/test/java/e2eTest.java
@@ -1,20 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
 import static ca.wbac.callbag.sink.Pullers.forEach;
 import static ca.wbac.callbag.source.Pullables.range;
 
 class e2eTest {
     @Test
     void test1() {
-        range(0, 1000000)
+        range(0, 10)
             .greet(forEach(System.out::println));
-    }
-
-    @Test
-    void stream() {
-        IntStream.range(0, 1000000)
-                .forEach(System.out::println);
     }
 }
