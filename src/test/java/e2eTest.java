a04fa21e37e0d58eae5af29a76b713b61db08d2c
mr-rampage <readonly@che.wbac.ca>
2018-10-27 22:19:37 -0400

    chore(imports) optimizing imports

--- src/test/java/e2eTest.java
+++ src/test/java/e2eTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
