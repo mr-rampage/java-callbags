ceb8fcae0e4ca1252d684584b188d27a202f1ced
mr-rampage <readonly@che.wbac.ca>
2018-11-04 00:33:48 -0400

    feat(skip operator)

---
+++ basics/src/main/java/ca/wbac/callbags/basics/operator/Skip.java
@@ -0,0 +1,38 @@
+package ca.wbac.callbags.basics.operator;
+
+import ca.wbac.callbags.core.Operator;
+import ca.wbac.callbags.core.SinkTalkback;
+import ca.wbac.callbags.core.SourceInitiator;
+import ca.wbac.callbags.core.SourceTalkback;
+
+public class Skip<T> implements Operator<T, T> {
+    private final int max;
+
+    public Skip(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public SourceInitiator<T> apply(SourceInitiator<T> inputSink) {
+        return outputSink -> inputSink.start(new SinkTalkback<>() {
+            private int skipped = 0;
+            private SourceTalkback source;
+
+            @Override
+            public void start(SourceTalkback sourceTalkback) {
+                source = sourceTalkback;
+                outputSink.start(sourceTalkback);
+            }
+
+            @Override
+            public void deliver(T data) {
+                if (skipped < max) {
+                    skipped++;
+                    source.request();
+                } else {
+                    outputSink.deliver(data);
+                }
+            }
+        });
+    }
+}
---
+++ basics/src/test/java/ca/wbac/callbags/basics/operator/SkipTest.java
@@ -0,0 +1,62 @@
+package ca.wbac.callbags.basics.operator;
+
+import ca.wbac.callbags.core.SinkTalkback;
+import ca.wbac.callbags.core.SourceInitiator;
+import ca.wbac.callbags.core.SourceTalkback;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SkipTest {
+    @Spy
+    private SourceInitiator<Integer> source;
+    @Spy
+    private SinkTalkback<Integer> sink;
+    @Spy
+    private SourceTalkback talkback;
+    @Captor
+    private ArgumentCaptor<SinkTalkback<Integer>> sinkCaptor;
+
+    private Skip<Integer> fixture = new Skip<>(3);
+
+    @BeforeEach
+    void whenStarted() {
+        fixture.apply(source).start(sink);
+        verify(source).start(sinkCaptor.capture());
+        sinkCaptor.getValue().start(talkback);
+    }
+
+    @Test
+    @DisplayName("should handshake")
+    void testHandshake() {
+        verify(sink).start(talkback);
+    }
+
+    @Test
+    @DisplayName("should not deliver when deliveries less than max")
+    void testSkipDelivery() {
+        sinkCaptor.getValue().deliver(5);
+        verify(sink, never()).deliver(any(Integer.class));
+        verify(talkback).request();
+    }
+
+    @Test
+    @DisplayName("should deliver when deliveries greater than max")
+    void testDelivery() {
+        sinkCaptor.getValue().deliver(1);
+        sinkCaptor.getValue().deliver(2);
+        sinkCaptor.getValue().deliver(3);
+        sinkCaptor.getValue().deliver(4);
+        verify(talkback, times(3)).request();
+        verify(sink).deliver(4);
+    }
+}
