0623752ea421cc348a4d85226aa8f5e4cebbda8a
mr-rampage <readonly@che.wbac.ca>
2020-03-09 23:50:56 -0400

    refactor(making class private)

--- basics/src/main/java/ca/wbac/callbags/basics/source/Flow.java
+++ basics/src/main/java/ca/wbac/callbags/basics/source/Flow.java
@@ -5,6 +5,8 @@ import ca.wbac.callbags.basics.ISink;
 import ca.wbac.callbags.basics.ISource;
 
 import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
 
 final class Flow<T, E> implements ISource<T, E> {
 
@@ -30,5 +32,41 @@ final class Flow<T, E> implements ISource<T, E> {
     @Override
     public void request() {
 
+    }
+
+    private final static class SinkSubscriber<T, E> implements Subscriber<T> {
+        private Subscription subscription;
+        private ISink<T, E> sink;
+
+        SinkSubscriber(final ISink<T, E> sink) {
+            this.sink = sink;
+        }
+
+        @Override
+        public void onSubscribe(final Subscription subscription) {
+            this.subscription = subscription;
+            this.subscription.request(Long.MAX_VALUE);
+        }
+
+        public void dispose() {
+            if (this.subscription != null) {
+                this.subscription.cancel();
+            }
+        }
+
+        @Override
+        public void onNext(final T item) {
+            this.sink.deliver(item);
+        }
+
+        @Override
+        public void onError(final Throwable throwable) {
+            this.sink.terminate();
+        }
+
+        @Override
+        public void onComplete() {
+            this.sink.terminate();
+        }
     }
 }
