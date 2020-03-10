c1973336e6ab2179306c6c5602855593c7034c33
mr-rampage <readonly@che.wbac.ca>
2020-03-10 00:05:33 -0400

    refactor(factories) - changed to interfaces

--- basics/src/main/java/ca/wbac/callbags/basics/sink/Sink.java
+++ basics/src/main/java/ca/wbac/callbags/basics/sink/Sink.java
@@ -4,8 +4,8 @@ import ca.wbac.callbags.basics.ISource;
 
 import java.util.function.Consumer;
 
-public final class Sink {
-    public static <T> Consumer<ISource<T>> forEach(final Consumer<T> consumer) {
+public interface Sink {
+    static <T> Consumer<ISource<T>> forEach(final Consumer<T> consumer) {
         var sink = new ForEach<>(consumer);
         return source -> source.greet(sink);
     }
--- basics/src/main/java/ca/wbac/callbags/basics/source/Source.java
+++ basics/src/main/java/ca/wbac/callbags/basics/source/Source.java
@@ -5,20 +5,20 @@ import ca.wbac.callbags.basics.ISink;
 import java.util.concurrent.Flow.Publisher;
 import java.util.function.Consumer;
 
-public final class Source {
-    public static Consumer<ISink<Integer>> range(final int lowerBound, final int upperBound) {
+public interface Source {
+    static Consumer<ISink<Integer>> range(final int lowerBound, final int upperBound) {
         return sink -> new Range(lowerBound, upperBound).greet(sink);
     }
 
-    public static Consumer<ISink<Integer>> interval(final long period) {
+    static Consumer<ISink<Integer>> interval(final long period) {
         return sink -> new Interval(period).greet(sink);
     }
 
-    public static <T> Consumer<ISink<T>> fromIter(final java.lang.Iterable<T> iterable) {
+    static <T> Consumer<ISink<T>> fromIter(final java.lang.Iterable<T> iterable) {
         return sink -> new Iterable<>(iterable).greet(sink);
     }
 
-    public static <T> Consumer<ISink<T>> fromFlow(final Publisher<T> publisher) {
+    static <T> Consumer<ISink<T>> fromFlow(final Publisher<T> publisher) {
         return sink -> new Flow<>(publisher).greet(sink);
     }
 }
